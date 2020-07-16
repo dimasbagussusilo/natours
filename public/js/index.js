47d63236845b215ad8ddf2314fb59e9ff880f79a
Dimas Bagus Susilo <dimas@codingbee.org>
2020-07-16 17:39:47 +0700

    Maintenance

--- public/js/index.js
+++ public/js/index.js
@@ -6,27 +6,40 @@ import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
 import { showAlert } from './alerts';
 
-// DOM ELEMENTS
-const mapBox = document.getElementById('map');
+// DOM Elements
+const mapbox = document.getElementById('map');
+const signupForm = document.querySelector('.form--signup');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
 const bookBtn = document.getElementById('book-tour');
 
-// DELEGATION
-if (mapBox) {
-  const locations = JSON.parse(mapBox.dataset.locations);
+// Delegation
+if (mapbox) {
+  const locations = JSON.parse(mapbox.dataset.locations);
   displayMap(locations);
 }
 
-if (loginForm)
+if (signupForm) {
+  signupForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    signup(name, email, password, passwordConfirm);
+  });
+}
+
+if (loginForm) {
   loginForm.addEventListener('submit', e => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
   });
+}
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
@@ -37,6 +50,7 @@ if (userDataForm)
     form.append('name', document.getElementById('name').value);
     form.append('email', document.getElementById('email').value);
     form.append('photo', document.getElementById('photo').files[0]);
+    console.log(form);
 
     updateSettings(form, 'data');
   });
